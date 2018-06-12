1ca9b26e8bee96af0631e4a65415dabb5b40b581
kees <cheuperman@gmail.com>
2018-06-12 11:28:44 +0100

    Fourth test

--- lib/notes-app.rb
+++ lib/notes-app.rb
@@ -5,3 +5,7 @@ end
 def show_notes
   return "x"
 end
+
+def display_note(x, y)
+  return ["x", "y"]
+end
--- spec/notes-app_spec.rb
+++ spec/notes-app_spec.rb
@@ -4,10 +4,13 @@ describe 'Notes_app' do
   it "Allows user to add title" do
     expect(add_note("x")).to eq("x")
   end
+  it "Allows user to add more notes" do
+    expect(add_note("y")).to eq("y")
+  end
   it "Shows user list of titles" do
     expect(show_notes).to eq("x")
   end
   it "Display title and body" do
-    expect(display_note("x","y")).to eq("x","y")
+    expect(display_note).to eq(["x", "y"])
   end
 end
